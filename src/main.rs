56470db01ebbb1795921f4935a2deef27b5161db
Chris Holcombe <xfactor973@gmail.com>
2017-12-21 11:52:11 -0800

    Fix queue starvation

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ extern crate log;
 extern crate scoped_threadpool;
 extern crate simplelog;
 
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::str::FromStr;
 
 use clap::{App, Arg};
@@ -19,6 +19,62 @@ use log::LogLevelFilter;
 use scoped_threadpool::Pool;
 use simplelog::{Config, TermLogger};
 
+// Print all the files in the directories from the queu
+fn print(queue: &MsQueue<PathBuf>, pool: &mut Pool, cluster: &mut Gluster) {
+    pool.scoped(|scoped| {
+        // Push directories onto the queue
+        scoped.execute(|| {
+            // TODO: This needs to try with a timeout
+            let p = match queue.try_pop() {
+                Some(p) => p,
+                None => {
+                    //done = true;
+                    return;
+                }
+            };
+            let sub_dir = GlusterDirectory { dir_handle: cluster.opendir(&p).unwrap() };
+            for s in sub_dir {
+                match s.file_type {
+                    DT_REG => {
+                        println!("{}/{}", p.display(), s.path.display());
+                    }
+                    _ => {
+                        //Nothing
+                    }
+                }
+            }
+        })
+    });
+}
+
+// Find all the directories and push them onto the queue
+fn find_dirs(queue: &MsQueue<PathBuf>, pool: &mut Pool, p: &Path, cluster: &mut Gluster) {
+    pool.scoped(|scoped| {
+        // Push directories onto the queue
+        scoped.execute(|| {
+            let this = Path::new(".");
+            let parent = Path::new("..");
+            //TODO: This needs to keep decending 
+            let sub_dir = GlusterDirectory { dir_handle: cluster.opendir(p).unwrap() };
+            for s in sub_dir {
+                match s.file_type {
+                    DT_DIR => {
+                        if !(s.path == this || s.path == parent) {
+                            // Push the dir onto the queue for another thread to handle
+                            let mut pbuff = PathBuf::from(p);
+                            pbuff.push(s.path);
+                            queue.push(pbuff);
+                        }
+                    }
+                    _ => {
+                        //Nothing
+                    }
+                }
+            }
+        })
+    });
+}
+
 fn list(
     server: &str,
     port: u16,
@@ -27,18 +83,20 @@ fn list(
     workers: u32,
 ) -> Result<(), GlusterError> {
     println!("Connecting to: {}", server);
-    let cluster = Gluster::connect(volume, server, port)?;
+    let mut cluster = Gluster::connect(volume, server, port)?;
 
     //Helpers to avoid adding self and parent paths
     let this = Path::new(".");
     let parent = Path::new("..");
 
-    let mut queue = MsQueue::new();
+    let queue = MsQueue::new();
     // Seed the queue with the first directory path
     queue.push(path.to_path_buf());
     let mut done = false;
 
     let mut worker_pool = Pool::new(workers);
+    find_dirs(&queue, &mut worker_pool, path, &mut cluster);
+    print(&queue, &mut worker_pool, &mut cluster);
 
     while !done {
         worker_pool.scoped(|scoped| {
@@ -47,12 +105,10 @@ fn list(
                 let p = match queue.try_pop() {
                     Some(p) => p,
                     None => {
-                        //println!("Queue empty");
                         done = true;
                         return;
                     }
                 };
-                //println!("try_pop: {}", p.display());
                 let sub_dir = GlusterDirectory { dir_handle: cluster.opendir(&p).unwrap() };
                 for s in sub_dir {
                     match s.file_type {
@@ -64,7 +120,6 @@ fn list(
                                 // Push the dir onto the queue for another thread to handle
                                 let mut pbuff = p.clone();
                                 pbuff.push(s.path);
-                                //println!("push dir: {}", pbuff.display());
                                 queue.push(pbuff);
                             }
                         }
@@ -75,8 +130,6 @@ fn list(
                 }
             });
         });
-
-        // What condition can I use to stop?
     }
     Ok(())
 }
